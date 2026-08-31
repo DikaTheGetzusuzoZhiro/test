992bc6200e89e5d47c2ab34e3ee95dd8c53e62f2
DikaTheGetzusuzoZhiro <dikasalto5@gmail.com>
2026-09-01 06:35:52 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['xxxxx.supabase.co'], // ganti dengan projectmu
-  },
-  // Hapus baris experimental.serverActions
-};
-
-module.exports = nextConfig;
